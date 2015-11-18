132f7b7a4c1d4a8dbc2bffc691d4f226f1d38e0a
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2015-11-18 12:26:38 -0800

    Functional queue is passing all core tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,17 +1,24 @@
 var Queue = function() {
   var someInstance = {};
   var queueSize = 0;
+  var queueStart = 0
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var key = queueSize + queueStart;
+    storage[key] = value;
     queueSize ++;
+
   };
 
   someInstance.dequeue = function() {
+    var dq = storage[queueStart];
     queueSize --;
+    queueStart ++;
+    return dq;
   };
 
   someInstance.size = function() {
