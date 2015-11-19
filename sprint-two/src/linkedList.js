3b23ba9dc09f6d3d0ccb5415363ba318e97298b0
nickkneafsey <nicholaskneafsey@gmail.com>
2015-11-19 10:38:15 -0800

    Linked List is passing all core tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,41 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.size = 0;
 
   list.addToTail = function(value) {
+    var link = Node(value);
+    if (this.size === 0) {
+      //create new node with value as the value
+      //set head, tail to new node
+      this.head = link;
+      this.tail = this.head;
+    }
+    else {
+      this.tail.next = link;
+      this.tail = link;
+    }
+    this.size++;
   };
 
   list.removeHead = function() {
+    var header = this.head;
+    if (this.size > 0) {
+      this.head = this.head.next;
+      this.size--;
+    }
+    return header.value;
   };
 
   list.contains = function(target) {
+    var walker = this.head;
+    while (walker !== null) {
+      if (walker.value === target) return true;
+      else {
+        walker = walker.next;
+      }
+    }
+    return false;
   };
 
   return list;
