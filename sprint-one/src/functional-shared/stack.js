5d2a735e9f94615d716a0f2e532310c1d5b68b75
nickkneafsey <nicholaskneafsey@gmail.com>
2015-11-18 18:08:31 -0800

    Functional shared stack is passing all core tests

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  storage.stackSize = 0;
+  extend(storage, stackMethods);
+  return storage;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value){
+  this[this.stackSize] = value;
+  this.stackSize++;
+};
+
+stackMethods.pop = function(){
+  var popped = this[this.stackSize-1];
+  delete this[this.stackSize-1];
+  this.stackSize--;
+  if (this.stackSize<0) this.stackSize = 0;
+  return popped; 
+};
+
+stackMethods.size = function(){
+  return this.stackSize;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
 
 
