b5034a45cb8551b9463d43fcf0237d7e57ee8d40
nickkneafsey <nicholaskneafsey@gmail.com>
2015-11-21 16:23:57 -0800

    Set works for objects and Numbers

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -18,10 +18,37 @@ describe('set', function() {
     expect(set.contains('Susan Sarandon')).to.equal(true);
   });
 
+
+
   it('should remove values from a set', function(){
     set.add("Mel Gibson");
     set.remove("Mel Gibson");
     expect(set.contains("Mel Gibson")).to.equal(false);
   });
 
+  it('should work with numbers', function(){
+    set.add(7);
+    set.add(8);
+    expect(set.contains(7)).to.equal(true);
+    expect(set.contains(8)).to.equal(true);
+  });
+
+  it('should remove numbers', function(){
+    set.add(52);
+    set.remove(52);
+    expect(set.contains(52)).to.equal(false);
+  });
+
+  it('should work with objects', function(){
+    set.add({test:"hi"});
+    set.add({nick:"kneafsey"});
+    expect(set.contains({nick:"kneafsey"})).to.equal(true);
+    expect(set.contains({test:"hi"})).to.equal(true);
+  });
+
+  it('should remove objects', function(){
+    set.add({test:"hi"});
+    set.remove({test:"hi"});
+    expect(set.contains({test:"hi"})).to.equal(false);
+  });
 });
